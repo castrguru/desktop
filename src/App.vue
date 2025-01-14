86bc799ebd4ec3c96b408b43d24ec9e27bd66669
Shomari <nyusternie@sdot.io>
2025-01-14 05:54:07 -0500

    Test conditionals.

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,52 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
+import { ref } from 'vue'
+import { invoke } from '@tauri-apps/api/core'
 
-const greetMsg = ref("");
-const name = ref("");
+const greetMsg = ref('')
+const name = ref('')
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", { name: name.value });
+    if (name.value === '') {
+        greetMsg.value = ''
+        return
+    }
+
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsg.value = await invoke('greet', { name: name.value })
 }
 </script>
 
 <template>
-  <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
+    <main class="container">
+        <h1>Welcome to Tauri + Vue</h1>
 
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <p>Click on the <span className="text-5xl font-bold">GURU</span>, Vite, and Vue logos to learn more.</p>
+        <div class="row">
+            <a href="https://vitejs.dev" target="_blank">
+                <img src="/vite.svg" class="logo vite" alt="Vite logo" />
+            </a>
+      
+            <a href="https://tauri.app" target="_blank">
+                <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
+            </a>
+      
+            <a href="https://vuejs.org/" target="_blank">
+                <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+            </a>
+        </div>
+    
+        <p>
+            Click on the <span className="text-5xl font-bold">GURU</span>, Vite, and Vue logos to learn more.
+        </p>
 
-    <form class="row" @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
-    </form>
-    <p>{{ greetMsg }}</p>
-  </main>
+        <form class="row" @submit.prevent="greet">
+            <input id="greet-input" v-model="name" placeholder="Enter a name..." />
+            <button type="submit">Greet</button>
+        </form>
+    
+        <p v-if="greetMsg.length > 1" className="p-5 bg-rose-200 text-2xl font-bold text-sky-800 tracking-widest leading-9 text-pretty">
+            {{ greetMsg }}
+        </p>
+    </main>
 </template>
 
 <style scoped>
